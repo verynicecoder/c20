285783714451c46f9a624e0590e5c209d18010bb
verynicecoder <78349778+verynicecoder@users.noreply.github.com>
2021-02-01 15:41:03 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+var movingRect , fixedRect;
+
+
+function setup() {
+  createCanvas(1200,800);
+  fixedRect=createSprite(200,200,50,80) ;
+  movingRect=createSprite(400,200,80,30);
+  fixedRect.shapeColor = "blue";
+movingRect.shapeColor = "green";
+}
+
+function draw() {
+  background(0);  
+  movingRect.x=World.mouseX;
+  movingRect.y=World.mouseY;
+
+  console.log(movingRect.x - fixedRect.x);
+  if(movingRect.x - fixedRect.x < fixedRect.width / 2 + movingRect.width/2
+    && fixedRect.x - movingRect.x < fixedRect.width / 2 + movingRect.width/2
+    && movingRect.y- fixedRect.y < fixedRect.height / 2 + movingRect.height/2
+    && fixedRect.y- movingRect.y < fixedRect.height / 2 + movingRect.height/2 ) {
+    fixedRect.shapeColor = "red";
+    movingRect.shapeColor = "red";
+  }
+
+  else
+  {
+    fixedRect.shapeColor = "blue";
+    movingRect.shapeColor = "green";
+
+  }
+  drawSprites();
+}
